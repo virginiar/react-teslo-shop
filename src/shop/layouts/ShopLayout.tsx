e3ad6678cba1b277814bc8f49bfa019368e9280f
Virginia Redondo <7237305+virginiar@users.noreply.github.com>
2026-02-11 10:40:32 +0100

    Implementando ShopLayout con componentes CustomHeader y CustomFooter y creando CustomJumbotron

--- src/shop/layouts/ShopLayout.tsx
+++ src/shop/layouts/ShopLayout.tsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router";
+import { CustomHeader } from "../components/CustomHeader";
+import { CustomFooter } from "../components/CustomFooter";
 
 export const ShopLayout = () => {
   return (
-    <div>
-      ShopLayout
+    <div className="min-h-screen bg-background">
+      <CustomHeader />
       <Outlet />
+      <CustomFooter />
     </div>
   );
 };
--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,14 +1,9 @@
-import { Button } from "@/components/ui/button";
+import { CustomJumbotron } from "@/shop/components/CustomJumbotron";
 
 export const HomePage = () => {
   return (
-    <div>
-      <h1>Hola Mundo</h1>
-      <h1 className="font-montserrat">Hola Mundo</h1>
-      <h1 className="font-montserrat font-thin">Hola Mundo</h1>
-      <h1 className="font-montserrat font-normal">Hola Mundo</h1>
-      <h1 className="font-montserrat font-bold">Hola Mundo</h1>
-      <Button>Hola mundo</Button>
-    </div>
+    <>
+      <CustomJumbotron title="Todos los productos" />
+    </>
   );
 };
