d5375acb1958b15d68d207b77abd0d34602148a6
Virginia Redondo <7237305+virginiar@users.noreply.github.com>
2026-02-11 12:07:45 +0100

    Implementando HomePage con los componentes FilterSidebar, ProductCard y ProductGrid usando un mock de products

--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,9 +1,15 @@
+import { CustomPagination } from "@/components/custom/CustomPagination";
+
+import { products } from "@/mocks/products.mock";
 import { CustomJumbotron } from "@/shop/components/CustomJumbotron";
+import { ProductsGrid } from "@/shop/components/ProductsGrid";
 
 export const HomePage = () => {
   return (
     <>
       <CustomJumbotron title="Todos los productos" />
+      <ProductsGrid products={products} />
+      <CustomPagination totalPages={7} />
     </>
   );
 };
