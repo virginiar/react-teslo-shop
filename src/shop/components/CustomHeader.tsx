a041ff2dcdae05f07877d8f07bd1397dff39f4db
Virginia Redondo <7237305+virginiar@users.noreply.github.com>
2026-02-13 10:02:32 +0100

    Añadiendo funcionalidad de búsqueda al componente CustomHeader

--- src/shop/components/CustomHeader.tsx
+++ src/shop/components/CustomHeader.tsx
@@ -1,10 +1,32 @@
-import { useState } from "react";
+import { useRef, useState, type KeyboardEvent } from "react";
+import { useSearchParams } from "react-router";
 import { Search, ShoppingBag, Menu } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
+
 export const CustomHeader = () => {
   const [cartCount] = useState(3);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  const query = searchParams.get("query") || "";
+
+  const handleSearch = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key !== "Enter") return;
+    const query = inputRef.current?.value;
+
+    const newSearchParams = new URLSearchParams();
+
+    if (!query) {
+      newSearchParams.delete("query");
+    } else {
+      newSearchParams.set("query", inputRef.current!.value);
+    }
+
+    setSearchParams(newSearchParams);
+  };
+
   return (
     <header className="sticky top-0 z-50 w-full border-b backdrop-blur bg-slate-50">
       <div className="container mx-auto px-4 lg:px-8">
@@ -54,7 +76,10 @@ export const CustomHeader = () => {
                 <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
                 <Input
                   placeholder="Buscar productos..."
-                  className="pl-9 w-64 h-9"
+                  className="pl-9 w-64 h-9 bg-white"
+                  ref={inputRef}
+                  onKeyDown={handleSearch}
+                  defaultValue={query}
                 />
               </div>
             </div>
