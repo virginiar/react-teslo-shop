016c3327569e3ca56ae7a2d92a16a470a282a2bd
Virginia Redondo <7237305+virginiar@users.noreply.github.com>
2026-02-24 19:00:04 +0100

    Implementando paginación

--- src/shop/actions/get-products.action.ts
+++ src/shop/actions/get-products.action.ts
@@ -1,8 +1,20 @@
 import { tesloApi } from "@/api/tesloApi";
 import type { ProductsResponse } from "@/interfaces/products.response";
 
-export const getProductsAction = async (): Promise<ProductsResponse> => {
-    const { data } = await tesloApi.get<ProductsResponse>('/products');
+
+interface Options {
+    limit?: number | string;
+    offset?: number | string;
+}
+
+export const getProductsAction = async (options: Options): Promise<ProductsResponse> => {
+    const { limit, offset } = options;
+    const { data } = await tesloApi.get<ProductsResponse>('/products', {
+        params: {
+            limit,
+            offset,
+        },
+    });
 
     const productsWithImageUrls = data.products.map((product) => ({
         ...product,
@@ -10,6 +22,7 @@ export const getProductsAction = async (): Promise<ProductsResponse> => {
             (image) => `${import.meta.env.VITE_API_URL}/files/product/${image}`
         ),
     }));
+
     return {
         ...data,
         products: productsWithImageUrls
--- src/shop/hooks/useProducts.tsx
+++ src/shop/hooks/useProducts.tsx
@@ -1,14 +1,24 @@
 import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "react-router";
 
 import { getProductsAction } from "../actions/get-products.action";
 
 export const useProducts = () => {
-  // TODO: viene lógica
+  // Lógica de paginación
+  const [searchParams] = useSearchParams();
+
+  const limit = searchParams.get("limit") || 9;
+  const page = searchParams.get("page") || 1;
+
+  const offset = (Number(page) - 1) * Number(limit);
   
   return useQuery({
-    queryKey: ["products"],
+    queryKey: ["products", { offset, limit }],
     queryFn: () =>
-      getProductsAction(),
+      getProductsAction({
+        limit: isNaN(+limit) ? 9 : limit,
+        offset: isNaN(offset) ? 0 : offset,
+      }),
     staleTime: 1000 * 60 * 5,
   });
 };
