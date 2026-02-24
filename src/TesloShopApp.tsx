a31aa80369f9f00cf6a4feb018d2402d5a3e880b
Virginia Redondo <7237305+virginiar@users.noreply.github.com>
2026-02-24 13:30:51 +0100

    Integrando TanStack Query y Axios

--- src/TesloShopApp.tsx
+++ src/TesloShopApp.tsx
@@ -1,6 +1,14 @@
 import { RouterProvider } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import { appRouter } from "./app.router";
 
+const queryClient = new QueryClient();
+
 export const TesloShopApp = () => {
-  return <RouterProvider router={appRouter} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={appRouter} />;
+    </QueryClientProvider>
+  );
 };
