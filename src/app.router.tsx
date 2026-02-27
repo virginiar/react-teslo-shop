a72fa9d1618e76702ab61fc322b12b48b7f590d9
Virginia Redondo <7237305+virginiar@users.noreply.github.com>
2026-02-27 17:39:31 +0100

    Implementando rutas protegidas

--- src/app.router.tsx
+++ src/app.router.tsx
@@ -12,6 +12,7 @@ import { RegisterPage } from "./auth/pages/register/RegisterPage";
 import { DashboardPage } from "./admin/pages/dashboard/DashboardPage";
 import { AdminProductPage } from "./admin/pages/product/AdminProductPage";
 import { AdminProductsPage } from "./admin/pages/products/AdminProductsPage";
+import { AdminRoute, NotAuthenticatedRoute } from "./components/routes/ProtectedRoutes";
 
 const AuthLayout = lazy(() => import("./auth/layouts/AuthLayout"));
 const AdminLayout = lazy(() => import("./admin/layouts/AdminLayout"));
@@ -40,7 +41,11 @@ export const appRouter = createBrowserRouter([
   // Auth Routes
   {
     path: "/auth",
-    element: <AuthLayout />,
+    element: (
+      <NotAuthenticatedRoute>
+        <AuthLayout />
+      </NotAuthenticatedRoute>
+    ),
     children: [
       {
         index: true,
@@ -59,7 +64,11 @@ export const appRouter = createBrowserRouter([
   // Admin Routes
   {
     path: "/admin",
-    element: <AdminLayout />,
+    element: (
+      <AdminRoute>
+        <AdminLayout />
+      </AdminRoute>
+    ),
     children: [
       {
         index: true,
---
+++ src/components/routes/ProtectedRoutes.tsx
@@ -0,0 +1,36 @@
+import { Navigate } from "react-router";
+import type { PropsWithChildren } from "react";
+
+import { useAuthStore } from "@/auth/store/auth.store";
+
+export const AuthenticatedRoute = ({ children }: PropsWithChildren) => {
+  const { authStatus } = useAuthStore();
+
+  if (authStatus === "checking") return null;
+
+  if (authStatus === "not-authenticated") return <Navigate to="/auth/login" />;
+
+  return children;
+};
+
+export const NotAuthenticatedRoute = ({ children }: PropsWithChildren) => {
+  const { authStatus } = useAuthStore();
+  
+  if (authStatus === "checking") return null;
+
+  if (authStatus === "authenticated") return <Navigate to="/" />;
+
+  return children;
+};
+
+export const AdminRoute = ({ children }: PropsWithChildren) => {
+  const { authStatus, isAdmin } = useAuthStore();
+
+  if (authStatus === "checking") return null;
+
+  if (authStatus === "not-authenticated") return <Navigate to="/auth/login" />;
+
+  if (!isAdmin()) return <Navigate to="/" />;
+
+  return children;
+};
