efa86fb3618e64534312bd5f0e73d9b6d8efbac4
Virginia Redondo <7237305+virginiar@users.noreply.github.com>
2026-02-27 16:41:25 +0100

    Implementando la comprobación del estado de autenticación con CheckAuthProvider, enlazando Zustand y TanStack

--- src/TesloShopApp.tsx
+++ src/TesloShopApp.tsx
@@ -1,16 +1,37 @@
 import { RouterProvider } from "react-router";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import type { PropsWithChildren } from "react";
+import { QueryClient, QueryClientProvider, useQuery } from "@tanstack/react-query";
 import { Toaster } from "sonner";
 
 import { appRouter } from "./app.router";
+import { useAuthStore } from "./auth/store/auth.store";
+import { CustomFullScreenLoading } from "./components/custom/CustomFullScreenLoading";
 
 const queryClient = new QueryClient();
+
+const CheckAuthProvider = ({ children }: PropsWithChildren) => {
+  const { checkAuthStatus } = useAuthStore();
+
+  const { isLoading } = useQuery({
+    queryKey: ["auth"],
+    queryFn: checkAuthStatus,
+    retry: false,
+    refetchInterval: 1000 * 60 * 1.5,
+    refetchOnWindowFocus: true,
+  });
+
+  if (isLoading) return <CustomFullScreenLoading />;
+  return children;
+};
 
 export const TesloShopApp = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <Toaster />
-      <RouterProvider router={appRouter} />
+      {/* Custom Provider */}
+      <CheckAuthProvider>
+        <RouterProvider router={appRouter} />
+      </CheckAuthProvider>
     </QueryClientProvider>
   );
 };
--- src/auth/store/auth.store.ts
+++ src/auth/store/auth.store.ts
@@ -2,11 +2,15 @@ import { create } from 'zustand';
 import type { User } from '@/interfaces/user.interface';
 
 import { loginAction } from '../actions/login.action';
+import { checkAuthAction } from '../actions/check-auth.actions';
+
+type AuthStatus = 'authenticated' | 'not-authenticated' | 'checking';
 
 type AuthStore = {
     // Propiedades
     user: User | null;
     token: string | null;
+    authStatus: AuthStatus;
 
     // Getters
 
@@ -14,13 +18,15 @@ type AuthStore = {
     // Acciones
     login: (email: string, password: string) => Promise<boolean>;
     logout: () => void;
+    checkAuthStatus: () => Promise<boolean>;
 };
 
 export const useAuthStore = create<AuthStore>()((set) => ({
     // Implementación de AuthStore
     user: null,
     token: null,
-
+    authStatus: 'checking',
+    
     // Acciones
     login: async (email: string, password: string) => {
         console.log({ email, password });
@@ -29,18 +35,42 @@ export const useAuthStore = create<AuthStore>()((set) => ({
             const data = await loginAction(email, password);
             localStorage.setItem('token', data.token);
 
-            set({ user: data.user, token: data.token });
+            set({ user: data.user, token: data.token, authStatus: 'authenticated' });
 
             return true;
+
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
         } catch (error) {
             localStorage.removeItem('token');
-            set({ user: null, token: null });
+            set({ user: null, token: null, authStatus: 'not-authenticated' });
             return false;
         }
     },
 
     logout: () => {
         localStorage.removeItem('token');
-        set({ user: null, token: null });
+        set({ user: null, token: null, authStatus: 'not-authenticated' });
+    },
+
+    checkAuthStatus: async () => {
+        try {
+            const { user, token } = await checkAuthAction();
+            set({
+                user: user,
+                token: token,
+                authStatus: 'authenticated',
+            });
+            return true;
+
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        } catch (error) {
+            set({
+                user: undefined,
+                token: undefined,
+                authStatus: 'not-authenticated',
+            });
+
+            return false;
+        }
     },
 }));
