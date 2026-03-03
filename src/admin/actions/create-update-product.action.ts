0e6929ad73cb9f2a62120f386ca204ef5fb8f89b
Virginia Redondo <7237305+virginiar@users.noreply.github.com>
2026-03-03 13:26:07 +0100

    Creando la acción createUpdateProductAction

---
+++ src/admin/actions/create-update-product.action.ts
@@ -0,0 +1,28 @@
+import { tesloApi } from '@/api/tesloApi';
+import type { Product } from '@/interfaces/product.interface';
+
+export const createUpdateProductAction = async (
+    productLike: Partial<Product>
+): Promise<Product> => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { id, user, images = [], ...rest } = productLike;
+
+    const isCreating = id === 'new';
+
+    rest.stock = Number(rest.stock || 0);
+    rest.price = Number(rest.price || 0);
+
+    const { data } = await tesloApi<Product>({
+        url: isCreating ? '/products' : `/products/${id}`,
+        method: isCreating ? 'POST' : 'PATCH',
+        data: rest,
+    });
+
+    return {
+        ...data,
+        images: data.images.map((image) => {
+            if (image.includes('http')) return image;
+            return `${import.meta.env.VITE_API_URL}/files/product/${image}`;
+        }),
+    };
+};
