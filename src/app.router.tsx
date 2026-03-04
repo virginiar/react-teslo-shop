6a8c9a7cb5dfcec3cced42d77da535feb42c569a
Virginia Redondo <7237305+virginiar@users.noreply.github.com>
2026-03-04 13:07:47 +0100

    Despliegue: Cambiando CreateBrowserRouter por CreateHashRouter

--- src/app.router.tsx
+++ src/app.router.tsx
@@ -1,5 +1,5 @@
 import { lazy } from "react";
-import { createBrowserRouter, Navigate } from "react-router";
+import { createHashRouter, Navigate } from "react-router";
 
 import { ShopLayout } from "./shop/layouts/ShopLayout";
 import { HomePage } from "./shop/pages/home/HomePage";
@@ -17,7 +17,7 @@ import { AdminRoute, NotAuthenticatedRoute } from "./components/routes/Protected
 const AuthLayout = lazy(() => import("./auth/layouts/AuthLayout"));
 const AdminLayout = lazy(() => import("./admin/layouts/AdminLayout"));
 
-export const appRouter = createBrowserRouter([
+export const appRouter = createHashRouter([
   // Main routes
   {
     path: "/",
